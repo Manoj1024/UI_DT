d27d5904d57b54cb0aacb17cb360cc9224f459a0
Manoj1024 <61429322+Manoj1024@users.noreply.github.com>
2022-07-29 03:38:43 +0530

    Delete uidt-3.js

--- uidt-3.js
+++
@@ -1,9 +0,0 @@
-function reverse(number) {
-  var reverse = 0;
-  while (number != 0) {
-    reverse = (reverse * 10) + (number % 10);
-    number = Math.floor(number / 10);
-  }
-  return reverse;
-}
-console.log(reverse(149));
